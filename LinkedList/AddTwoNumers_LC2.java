af116c98308f02cbe6cd84e200a927466ced36ae
Varun Kumar <varunkumar6807@gmail.com>
2020-07-19 22:53:48 -0500

    Create AddTwoNumers_LC2.java

---
+++ LinkedList/AddTwoNumers_LC2.java
@@ -0,0 +1,26 @@
+class AddTwoNumers_LC2 {
+    //Varun Kumar
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr=dummy;
+        int carry=0;
+        while(l1!=null || l2!=null){
+            int x = l1!=null ? l1.val : 0;
+            int y = l2!=null ? l2.val : 0;
+            int sum = x+y+carry;
+            curr.next = new ListNode(sum%10);
+            carry=sum/10;
+            curr=curr.next;
+            if(l1!=null){
+                l1=l1.next;
+            }
+            if(l2!=null){
+                l2=l2.next;
+            }
+        }
+        if(carry==1){
+            curr.next = new ListNode(1);
+        }
+        return dummy.next;
+    }
+}
